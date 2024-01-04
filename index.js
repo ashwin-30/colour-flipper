65d84c6ef84e2b18b0b2f7fbc609ef8a5a61fb7e
ashwin-30 <ashwin3082003@gmail.com>
2024-01-04 21:22:23 +0530

    added js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const colors=["green","yellow","blue","orange","black","red"];
+const colors = ["green", "yellow", "blue", "orange", "red", "purple", "pink", "brown", "cyan"];
 
-var gbtn = $("btn");
-console.log(gbtn);
+// Add an event listener to the "Reload" button
+$(".yellow").on("click", function () {
+    location.reload(); // This will reload the page
+});
+
+// Add an event listener to the "Flipper" button
+$(".green").on("click", function () {
+    var randomNum = randomNumber();
+    $("body").css("background-color", colors[randomNum]);
+    $(".clr").text(colors[randomNum]);
+});
+
+function randomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
